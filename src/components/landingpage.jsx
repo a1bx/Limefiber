e1f06e6cf4c0bc25fe51c8500a1fff602db3fc38
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2023-12-14 13:11:41 +0300

    home page mobile view

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -18,7 +18,7 @@ const Home = () => {
           <div>
             <div className="text text-5xl font-semibold font-['Baloo Da 2'] leading-[81px] mb-2 p-10 ml-10">
               We provide Home Fiber Internet, quick and <br></br>reliable!
-              <p className="w-[555px] h-[62px] text-black text-lg font-normal font-['Baloo Da 2'] leading-7 mt-4">
+              <p className="w-[555px] h-[62px] text-black text-lg font-normal font-['Baloo Da 2'] leading-7 mt-4 ">
                 Our goal is to provide unlimited internet and offer a simple,
                 stable browsing experience.
               </p>
@@ -45,7 +45,24 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <img src={Banner} className='block md:hidden  rounded w-[95%] mt-10 h-[300px] mx-auto' />
+      <img src={Banner} className='block md:hidden rounded w-[95%] mt-10 h-[300px] mx-auto' />
+      <div className="ml-5 sm:hidden p-2 grid grid-flow-row mx-auto">
+              <p className="text-black font-normal font-['Baloo Da 2'] text-base max-w-full my-10 md:mb-0 md:mt-10 p-0 md:p-10 text-center">
+                Our goal is to provide unlimited internet and offer a simple,
+                stable browsing experience.
+              </p>
+              <div className="w-[169px] h-[60px] justify-center place-items-center mt-2 relative">
+                <div className="w-[94px] left-[50px] top-[15px] absolute text-black text-lg font-semibold font-['Baloo Da 2'] leading-7">
+                  Learn More
+                </div>
+                <div className="w-[60px] h-[60px] left-0 top-0 absolute">
+                  <img
+                    className="w-[30px] h-[60px] left-0 top-0 absolute"
+                    src={Learn}
+                  />
+                </div>
+              </div>
+            </div>
     </>
   );
 }
