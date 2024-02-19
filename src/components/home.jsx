1579ae529f3b0b25f77271045e185c0de3bfe271
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-02-19 19:55:42 +0300

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -44,19 +44,19 @@ const Home = () => {
         {/* <Navigate to='/home' /> */}
         <div className="flex flex-col w-full lg:flex-row">
           <div>
-            <div className="text text-5xl font-semibold font-['Baloo Da 2'] leading-[81px] mb-2 p-10 ml-10">
+            <div className="text-5xl font-semibold font-Baloo-Da-2 leading-[81px] mb-2 p-10 ml-10">
               We provide Home Fiber Internet, quick and <br></br>reliable!
-              <p className="w-[555px] h-[62px] text-black text-lg font-normal font-['Baloo Da 2'] leading-7 mt-4 ">
+              <p className="w-[555px] h-[62px] text-black text-lg font-normal font-Baloo-Da-2 leading-7 mt-4 ">
                 Our goal is to provide unlimited internet and offer a simple,
                 stable browsing experience.
               </p>
             </div>
             <div className="ml-10 p-10 grid grid-cols-4 mt-10">
-              <button className="bg-green text-white rounded-md hover:text-white">
+              <button className="bg-green text-white rounded-md hover:text-white ">
                 Get Started
               </button>
               <div className="w-[169px] h-[60px] left-[50px] relative">
-                <div className="w-[94px] left-[50px] top-[15px] absolute text-black text-lg font-semibold font-['Baloo Da 2'] leading-7">
+                <div className="left-[50px] top-[15px] absolute text-black text-lg font-semibold font-Baloo-Da-2 leading-7">
                   Learn More
                 </div>
                 <div className="w-[60px] h-[60px] left-0 top-0 absolute">
