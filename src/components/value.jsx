414f95e5d5f301536774179c9ac197d30614cbec
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-02-23 01:06:38 +0300

    update

--- src/components/value.jsx
+++ src/components/value.jsx
@@ -13,7 +13,7 @@ const Value = () => {
       <div className="text-center text-zinc-500 text-xs font-normal leading-7 tracking-[6.30px] mb-2">
         VALUE PROPOSITION
       </div>
-      <h1 className="text-center text-2xl md:text-4xl font-semibold font-Baloo-Da-2">
+      <h1 className="text-center text-2xl md:text-4xl font-semibold font-Baloo">
         LimeFiber Vision and Goals
       </h1>
       <div className="flex flex-col sm:flex-row md:mx-20 mx-auto gap-4 p-5 mt-8 justify-items-center">
@@ -23,11 +23,11 @@ const Value = () => {
             alt="Icon 1"
             className="w-[65px] h-[65px] mx-auto mb-2"
           />
-          <h2 className="font-semibold text-[20px] md:text-[23px] lg:text-[26px] text-center font-Baloo-Da-2 leading-7">
+          <h2 className="font-semibold text-[20px] md:text-[23px] lg:text-[26px] text-center font-Baloo leading-7">
             Unlimited Internet
           </h2>
           <p
-            className="text-base max-w-full my-10 md:mb-0 md:mt-10 p-0 md:p-10 text-center"
+            className="text-base max-w-full my-10 md:mb-0 md:mt-10 p-0 md:p-10 font-Baloo text-center"
             style={{ color: "#707070" }}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dui
@@ -35,7 +35,7 @@ const Value = () => {
             laoreet dolor nec imperdiet.
           </p>
           <div className="flex justify-center">
-          <button className="md:ml-20 px-4 py-2 place-self-center rounded-md bg-green hover:bg-green hover:text-white text-white">
+          <button className="md:ml-20 px-4 py-2 place-self-center font-Baloo rounded-md bg-green hover:bg-green hover:text-white text-white">
             Learn More
           </button>
           </div>
@@ -52,11 +52,11 @@ const Value = () => {
             alt="Icon 2"
             className="w-[65px] h-[65px] mx-auto mb-2"
           />
-          <h2 className="font-semibold text-[20px] md:text-[23px] lg:text-[26px] text-center font-Baloo-Da-2 leading-7">
+          <h2 className="font-semibold text-[20px] md:text-[23px] lg:text-[26px] text-center font-Baloo leading-7">
             Simple and Secure
           </h2>
           <p
-            className="text-base max-w-full my-10 md:mt-10 md:mb-0 p-0 md:p-10 text-center"
+            className="text-base max-w-full my-10 md:mt-10 md:mb-0 p-0 md:p-10 font-Baloo text-center"
             style={{ color: "#707070" }}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dui
@@ -64,7 +64,7 @@ const Value = () => {
             laoreet dolor nec imperdiet.
           </p>
           <div className="flex justify-center">
-          <button className="flex place-self-center md:ml-20 px-4 py-2 rounded-md bg-green hover:bg-green hover:text-white text-white">
+          <button className="flex place-self-center md:ml-20 px-4 py-2 font-Baloo rounded-md bg-green hover:bg-green hover:text-white text-white">
             Learn More
           </button>
           </div>
@@ -81,11 +81,11 @@ const Value = () => {
             alt="Icon 3"
             className="w-[65px] h-[65px] mx-auto mb-2  "
           />
-          <h2 className="font-semibold text-[20px] md:text-[23px] lg:text-[26px] text-center font-Baloo-Da-2 leading-7">
+          <h2 className="font-semibold text-[20px] md:text-[23px] lg:text-[26px] text-center font-Baloo leading-7">
             Customer Support
           </h2>
           <p
-            className="text-base max-w-full my-10 md:mb-0 md:mt-10 p-0 md:p-10 text-center"
+            className="text-base max-w-full my-10 md:mb-0 md:mt-10 p-0 font-Baloo md:p-10 text-center"
             style={{ color: "#707070" }}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dui
@@ -93,7 +93,7 @@ const Value = () => {
             laoreet dolor nec imperdiet.
           </p>
           <div className='flex justify-center'>
-          <button className="md:ml-20 place-self-center mx-auto px-4 py-2 rounded-md bg-green hover:bg-green hover:text-white text-white">
+          <button className="md:ml-20 place-self-center mx-auto px-4 py-2 font-Baloo rounded-md bg-green hover:bg-green hover:text-white text-white">
             Learn More
           </button>
           </div>
@@ -105,16 +105,16 @@ const Value = () => {
           <div className="text-sm font-normal font-['Baloo Da 2'] leading-7 tracking-[6.30px]">
             COVERAGE
           </div>
-          <div className="w-85% wrap-text text-[20px] font-bold font-Baloo-Da-2 leading-loose">
+          <div className="w-85% wrap-text text-[20px] font-bold font-Baloo leading-loose">
             Check your Home Fiber Coverage
           </div>
-          <div className="w-[322.05px] h-[131.77px] text-opacity-40 text-xl font-normal font-Baloo-Da-2 leading-relaxed mt-4">
+          <div className="w-[322.05px] h-[131.77px] text-opacity-40 text-xl font-normal font-Baloo leading-relaxed mt-4">
             Find out if your area is Home Fiber Ready! To begin click the button
             below
           </div>
 
           <div className="flex md:grid-flow-col grid-flow-row px-2 py-2 gap-10 mt-2 md:mt-10 ">
-            <button className="left-0 px-4 py-2 rounded-full border text-green hover:bg-green hover:text-white mt-4">
+            <button className="left-0 px-4 py-2 font-Baloo rounded-full border text-green hover:bg-green hover:text-white mt-4">
               Get started
             </button>
             <img
