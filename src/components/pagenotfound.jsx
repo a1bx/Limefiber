d47f81b0096e6ac7ea55672ac1e157b091889e4e
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-02-27 12:25:16 +0300

    not found page

--- src/components/pagenotfound.jsx
+++ src/components/pagenotfound.jsx
@@ -3,22 +3,22 @@ import Warning from "../assets/warning.svg";
 
 function NotFound () {
     return (
-        <div className="container mx-auto mb-10">
+        <div className="relative justify-items-center mx-auto h-screen p-4">
             <div className="grid md:grid-cols-1">
                 <h3 className="text-center font-bruno font-medium text-black text-[32px] mt-[10rem]">404</h3>
 
                 <img className="mx-auto"
-                     alt="warning"
-                     src={Warning}
+                    alt="warning"
+                    src={Warning}
                 />
 
                 <h3 className="text-center font-baloo font-semibold text-black text-[32px]">Page not found</h3>
 
-                <p className="text-center font-baloo font-normal text-black text-[18px]">
+                <p className="text-center font-baloo font-normal text-black text-[15px]">
                     Lorem ipsum duis dolor sit amet, consectetur <br/> adipiscing elitvestibulum in pharetra.
                 </p>
 
-                <a href="/" className="mx-auto mt-4 flex justify-center items-center w-[162px] h-[49px] bg-[#75a774] rounded-md font-baloo font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
+                <a href="/" className="mx-auto mt-4 flex justify-center items-center w-[162px] h-[49px] bg-green rounded-md font-baloo font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
                     Go Back Home
                 </a>
             </div>
