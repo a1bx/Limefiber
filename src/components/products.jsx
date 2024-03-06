99396e9d1a673a91b3b36a324f2d866553eda353
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-03-06 13:33:10 +0300

    responsiveness

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -10,7 +10,6 @@ import Router from "../assets/router.svg";
 import Line from "../assets/Icon color.svg";
 import Map from "../assets/map.svg";
 import Navbar from "./navbar";
-import Footer from "./footer";
 
 const Products = () => {
     const slides = [Banner2, Banner3, Banner1];
@@ -24,85 +23,13 @@ const Products = () => {
       setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
   };
   return (
-    <>
-    <Navbar></Navbar>
     <div className="bg-white flex flex-row justify-center w-full">
-      <div className="overflow-hidden w-[1440px] h-[3604px] relative">
-        <img className="w-[1248px] top-[2709px] left-[87px] absolute h-px object-cover" alt="Line" src={Line} />
-        <div className="relative md:absolute w-[1128px] h-[1503px] top-[1135px] left-[179px]">
-          <div className="absolute w-[163px] top-[661px] left-[760px] [font-family:'Baloo_Da_2-Medium',Helvetica] font-medium text-light-colorsblack-light text-[38px] tracking-[0] leading-[43px] whitespace-nowrap">
-            Products
-          </div>
-          <div className="absolute w-[194px] top-[1168px] left-[143px] [font-family:'Baloo_Da_2-Medium',Helvetica] font-medium text-light-colorsblack-light text-[38px] tracking-[0] leading-[43px] whitespace-nowrap">
-            Services
-          </div>
-          <p className="absolute w-[500px] top-[168px] left-0 [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-[#6f6f6f] text-[17px] text-center tracking-[0] leading-[normal]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat dolor odio odio malesuada at condimentum
-            adipiscing iaculis semper.
-          </p>
-          <p className="top-[731px] left-[605px] absolute w-[500px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-[#6f6f6f] text-[17px] text-center tracking-[0] leading-[normal]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat dolor odio odio malesuada at condimentum
-            adipiscing iaculis semper.
-          </p>
-          <p className="top-[1241px] left-0 absolute w-[500px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-[#6f6f6f] text-[17px] text-center tracking-[0] leading-[normal]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat dolor odio odio malesuada at condimentum
-            adipiscing iaculis semper.
-          </p>
-          <div className="absolute w-[164px] h-[49px] top-[1345px] left-[144px]">
-            <div className="absolute w-[81px] top-[13px] left-[30px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-[#75a774] text-[16px] tracking-[0] leading-[normal] whitespace-nowrap">
-              View more
-            </div>
-            <button className="all-[unset] box-border absolute w-[164px] h-[49px] top-0 left-0">
-              <div className="relative w-[162px] h-[49px] bg-[#75a774] rounded-[10px]">
-                <div className="absolute w-[94px] top-[11px] left-[34px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
-                  View More
-                </div>
-                <AiOutlineRight className="!absolute !w-[21px] !h-[18px] !top-[16px] !left-[117px]" color="white" />
-              </div>
-            </button>
-            <AiOutlineRight className="!top-[19px] !left-[133px] !absolute !w-[21px] !h-[18px]" color="#75A774" />
-          </div>
-          <div className="absolute w-[164px] h-[49px] top-[835px] left-[750px]">
-            <button className="all-[unset] box-border absolute w-[164px] h-[49px] top-0 left-0">
-              <div className="relative w-[162px] h-[49px] bg-[#75a774] rounded-[10px]">
-                <div className="absolute w-[94px] top-[11px] left-[34px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
-                  View More
-                </div>
-                <AiOutlineRight className="!absolute !w-[21px] !h-[18px] !top-[16px] !left-[117px]" color="white" />
-              </div>
-            </button>
-            <AiOutlineRight className="!top-[20px] !left-[126px] !absolute !w-[21px] !h-[18px]" color="#75A774" />
-          </div>
-          <img
-            className="top-0 left-[601px] absolute w-[509px] h-[430px] object-cover"
-            alt="Rectangle"
-            src={Bundled}
-          />
-          <img
-            className="top-[536px] left-0 absolute w-[509px] h-[430px] object-cover"
-            alt="Rectangle"
-            src={Router}
-          />
-          <img
-            className="top-[1073px] left-[605px] absolute w-[509px] h-[430px] object-cover"
-            alt="Rectangle"
-            src={Services}
-          />
-          <div className="absolute w-[320px] top-[95px] left-[95px] [font-family:'Baloo_Da_2-Medium',Helvetica] font-medium text-light-colorsblack-light text-[38px] tracking-[0] leading-[43px] whitespace-nowrap">
-            Bundled packages
-          </div>
-          <button className="all-[unset] box-border absolute w-[164px] h-[49px] top-[295px] left-[169px]">
-            <div className="relative w-[162px] h-[49px] bg-[#75a774] rounded-[10px]">
-              <div className="absolute w-[94px] top-[11px] left-[34px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
-                View More
-              </div>
-              <AiOutlineRight className="absolute w-[21px] h-[18px] top-[16px] left-[117px]" color="white" />
-            </div>
-          </button>
-        </div>
+      
+      <div className="md:w-[1440px] md:h-[3604px] relative">
+        <Navbar/>
         <div className="relative h-[50vh] md:h-[90vh] bg-cover bg-center flex items-center text-white">
-    <div className="text-center z-10">
-    <div className="absolute top-20 p-4 md:top-[242px] md:left-[747px] font-baloo font-medium text-white text-[20px] md:text-[45px] tracking-[0] leading-[43px] whitespace-nowrap">
+          <div className="text-center z-10">
+            <div className="absolute top-20 p-4 md:top-[242px] md:left-[747px] font-baloo font-medium text-white text-[20px] md:text-[45px] tracking-[0] leading-[43px] whitespace-nowrap">
         LimeFiber Products and Services
         </div>
         <div className="md:w-[419px] h-px">
@@ -113,14 +40,14 @@ const Products = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat dolor odio odio malesuada at condimentum adipiscing iaculis semper.
             </p>
         </div>
-    </div>
-    <div className="absolute inset-0">
+        </div>
+        <div className="absolute inset-0">
         <img
         src={slides[currentSlide]}
         className="object-cover w-screen h-full p-2"
         alt="Banner"
         />
-    </div>
+        </div>
     {/* < className="mt-80 flex items-center justify-center absolute ml-40"> */}
     <button onClick={handlePrev} className="bg-white rounded h-20 bottom-[40vh] p-2 right-[170vh] mt-80 flex items-center justify-center absolute ml-40">
         <AiOutlineLeft style={{ fontSize: "2em", color: "#75A774" }} />
@@ -128,9 +55,85 @@ const Products = () => {
     <button onClick={handleNext} className="bg-white rounded h-20 bottom-[40vh] p-2 left-[150vh] mt-80 flex items-center justify-center absolute ml-40">
         <AiOutlineRight style={{ fontSize: "2em", color: "#75A774" }} />
     </button>
+    </div>
+        
+        <div className="relative">
+          
+          {/* bundled packages */}
+          <div className="absolute mt-10 justify-center ">
+          <img
+            className="p-4 w-full mt-2 md:left-[601px] md:w-[509px] md:h-[430px]"
+            alt="Rectangle"
+            src={Bundled}
+          />
+          <p className="relative text-center text-[24px] md:w-[320px] md:top-[18vh] md:left-[95px] font-Baloo font-medium md:text-[38px]">
+            Bundled packages
+          </p>
+          <p className="relative p-2 md:w-[500px] md:top-[168px] left-0 [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-[#6f6f6f] text-[17px] text-center tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat dolor odio odio malesuada at condimentum
+            adipiscing iaculis semper.
+          </p>
+          <div className="flex justify-center">
+          <button className="mt-10 md:top-[295px] md:left-[169px] relative w-[162px] h-[49px] bg-green rounded-2xl md:rounded-lg">
+              <p className="absolute w-[94px] top-[11px] left-[34px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
+                View More
+              </p>
+              <AiOutlineRight className="absolute w-[21px] h-[18px] top-[17px] left-[127px]" color="white" />
+          </button>
+          </div>
+          </div>
 
-</div>
-        <div className="absolute w-[1170px] top-[2796px] left-[144px] grid grid-cols-2 gap-4">
+          {/* products */}
+          <div className="">
+          <p className="relative md:w-[163px] top-[120vh] md:left-[760px] font-Baloo text-[24px] font-medium text-center md:text-[38px] tracking-[0] leading-[43px]  ">
+            Products
+          </p>
+          <p className="relative top-[120vh] md:top-[731px] md:left-[605px] md:absolute md:w-[500px] font-Baloo font-normal text-[#6f6f6f] text-[17px] text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat dolor odio odio malesuada at condimentum
+            adipiscing iaculis semper.
+          </p>
+          <div className="flex justify-center">
+            <button className="top-[125vh] relative w-[164px] h-[49px] md:left-[750px] md:top-[835px] bg-green rounded-2xl">
+                <p className="absolute w-[94px] top-[11px] left-[34px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
+                  View More
+                </p>
+                <AiOutlineRight className="absolute w-[21px] h-[18px] top-[17px] left-[127px]" color="white" />
+              </button>
+            </div>
+            <img
+            className="top-[80vh] w-full p-4 left-0 absolute md:w-[509px] md:h-[430px] object-cover"
+            alt="Rectangle"
+            src={Router}
+          />
+          </div>
+
+          {/* services */}
+          <div>
+          <p className="relative text-center top-[169vh] md:w-[194px] md:top-[1168px] md:left-[143px] font-Baloo font-medium text-light-colorsblack-light text-[24px] md:text-[38px] tracking-[0] leading-[43px] whitespace-nowrap">
+            Services
+          </p>
+          <p className="top-[170vh] md:top-[1241px] left-0 relative md:w-[500px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-[#6f6f6f] text-[17px] text-center tracking-[0] leading-[normal]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat dolor odio odio malesuada at condimentum
+            adipiscing iaculis semper.
+          </p>
+          <div className="flex justify-center">
+              <button className="relative top-[174vh] w-[162px] h-[49px] bg-green rounded-2xl md:top-[1345px] md:left-[144px]">
+                <p className="absolute w-[94px] top-[11px] left-[34px] [font-family:'Baloo_Da_2-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[28.5px]">
+                  View More
+                </p>
+                <AiOutlineRight className="!absolute !w-[21px] !h-[18px] !top-[16px] !left-[117px]" color="white" />
+              </button>
+            </div>
+              <img
+            className="md:top-[1073px] w-full p-4 top-[155vh] md:left-[605px] absolute md:w-[509px] md:h-[430px] object-cover"
+            alt="Rectangle"
+            src={Services}
+          />
+          </div>
+          
+        </div>
+        <img className="md:w-[1248px] md:top-[2709px] md:left-[87px] absolute h-px object-cover" alt="Line" src={Line} />
+        <div className="md:absolute relative top-[190vh] p-4 md:w-[162.5vh] md:top-[340vh] md:left-[10vh] grid grid-cols-2 gap-4">
           <div className="relative w-[327px] h-[302px]">
             <button className="px-4 py-2 font-Baloo rounded-full border text-green hover:bg-green hover:text-white mt-4 !h-[51px] !absolute !left-[2px] !w-[133px] !top-[246px]">
               Get started
@@ -143,16 +146,14 @@ const Products = () => {
                 Find out if your area is Home Fiber Ready! To begin click the button below
               </p>
             </div>
-            <img className="absolute w-[67px] h-[68px] top-[234px] left-[200px] object-cover cursor-pointer" src={Map} />
+            <img className="absolute md:relative h-[300px] md:top-[260px] opacity-40 md:opacity-100 right-[-160px] md:h-[60px] bottom-0 ml-10" src={Map} />
           </div>
           <div className="relative">
-            <img className="w-[687.53px] h-[405.44px] rounded-[10px]" src={Icon} alt="Icon 2" />
+            <img className="hidden md:block w-[687.53px] h-[405.44px] rounded-[10px]" src={Icon} alt="Icon 2" />
           </div>
         </div>
       </div>
     </div>
-    <Footer></Footer>
-    </>
   );
 };
 export default Products;
