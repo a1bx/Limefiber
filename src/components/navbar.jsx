d858e713f81d8121400fbd983c66a7194cfd7407
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-03-07 14:05:26 +0300

    cleanup

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -38,16 +38,16 @@ const Navbar = () => {
           isOpen ? "block" : "hidden"
         } lg:flex lg:items-center lg:w-auto`}
       >
-        <div className="hidden text-red text-lg font-normal font-['Baloo Da 2'] leading-7">
+        {/* <div className="hidden text-red text-lg font-normal font-['Baloo Da 2'] leading-7">
           Home
-        </div>
+        </div> */}
         <div className="md:hidden w-80 h-[692px] relative bg-white rounded-[10px]">
           <div className="w-72 h-[0px] left-[16px] top-[480px] absolute border border-zinc-300"></div>
           <div className="w-[129px] h-[460px] left-[16px] top-[119px] absolute">
             <div className="left-0 top-[50px] absolute">
               <a
                 href="products"
-                className="text-black text-xl font-normal font-['Baloo Da 2'] leading-relaxed"
+                className="text-black text-xl font-normal font-Baloo leading-relaxed"
               >
                 Products
               </a>
@@ -55,28 +55,33 @@ const Navbar = () => {
             <div className="left-0 top-[100px] absolute">
               <a
                 href="about"
-                className="text-black text-xl font-normal font-['Baloo Da 2'] leading-relaxed"
+                className="text-black text-xl font-normal font-Baloo leading-relaxed"
               >
                 About
               </a>
             </div>
-            <div className="left-0 top-[150px] absolute text-black text-xl font-normal font-['Baloo Da 2'] leading-relaxed">
+            <div className="left-0 top-[150px] absolute text-black text-xl font-normal font-Baloo leading-relaxed">
               <a href="blog">Blog</a>
             </div>
-            <div className="left-0 top-[196px] absolute text-black text-xl font-normal font-['Baloo Da 2'] leading-relaxed">
+            <div className="left-0 top-[196px] absolute text-black text-xl font-normal font-Baloo leading-relaxed">
               Help
             </div>
-            <div className="left-0 top-[246px] absolute text-black text-xl font-normal font-['Baloo Da 2'] leading-relaxed">
+            <div className="left-0 top-[246px] absolute text-black text-xl font-normal font-Baloo leading-relaxed">
               Contact
             </div>
-            <div className="left-0 top-[296px] absolute text-black text-xl font-normal font-['Baloo Da 2'] leading-relaxed">
+            <div className="left-0 top-[296px] absolute text-black text-xl font-normal font-Baloo leading-relaxed">
               Search
             </div>
-            <div className="left-0 top-0 absolute text-black text-xl font-normal font-['Baloo Da 2'] leading-relaxed">
-              Home
+            <div className="left-0 top-0 absolute text-black text-xl font-normal font-Baloo leading-relaxed">
+            <a
+                href="/"
+                className="text-black text-xl font-normal font-Baloo leading-relaxed"
+              >
+                Home
+              </a>
             </div>
             <div className="w-[129px] h-[26px] left-0 top-[385px] absolute">
-              <div className="left-[30px] top-0 absolute text-black text-lg font-normal font-['Baloo Da 2'] leading-relaxed">
+              <div className="left-[30px] top-0 absolute text-black text-lg font-normal font-Baloo leading-relaxed">
                 My account
               </div>
               <img
@@ -86,7 +91,7 @@ const Navbar = () => {
               />
             </div>
             <div className="w-[92px] h-[26px] left-0 top-[434px] absolute">
-              <div className="left-[30px] top-0 absolute text-black text-lg font-normal font-['Baloo Da 2'] leading-relaxed">
+              <div className="left-[30px] top-0 absolute text-black text-lg font-normal font-Baloo leading-relaxed">
                 Logout
               </div>
               <img
@@ -100,7 +105,7 @@ const Navbar = () => {
           <div className="w-72 h-8 left-[16px] top-[48px] absolute">
             {/* <div className="w-72 h-8 left-0 top-0 absolute bg-gray rounded" /> */}
             <input
-              className="left-[30px] w-72 h-8 top-[6px] absolute text-sm font-normal font-['Baloo Da 2'] leading-tight border-none bg-gray rounded"
+              className="left-[30px] w-72 h-8 top-[6px] absolute text-sm font-normal font-Baloo leading-tight border-none bg-gray rounded"
               type="text"
               placeholder="Search"
             />
@@ -111,31 +116,31 @@ const Navbar = () => {
             />
           </div>
         </div>
-        <div className="w-[188px] h-[49px] md:ml-[512px] top-[-12vh] p-6 ml-3 md:top-5 md:p-2 relative">
-          <button className="left-0 top-[10px] absolute text-black text-lg font-normal font-['Baloo Da 2'] leading-7 md:cursor-pointer">
+        <div className="w-[188px] h-[49px] md:ml-[512px] top-[-12vh] p-6 ml-3 md:top-[-4vh] md:p-2 relative md:flex md:left-[40vh]">
+          <button className="left-0 top-[10px] absolute text-black text-lg font-normal font-Baloo hover:text-red leading-7 md:cursor-pointer">
             Log in
           </button>
           {/* ">
             <div className="w-[119px] h-[49px] left-0 top-0 absolute " />
             <button className="left-[30px] top-[10px] absolute text-white text-lg font-normal font-['Baloo Da 2'] leading-7">Sign up</button>
             </div> */}
-          <button className="w-[119px] h-[49px] left-[69px] top-0 absolute bg-green cursor-pointer rounded-[10px] text-white">
+          <button className="w-[119px] h-[49px] left-[69px] top-0 absolute bg-green font-Baloo hover:text-red cursor-pointer rounded-[10px] text-white">
             sign up
           </button>
         </div>
       </div>
-      <div className="w-screen hidden h-[49px] md:block relative">
-        <div className="h-[29px] left-0 top-[15px] absolute">
-          <div className="left-0 top-0 absolute hover:text-red cursor-pointer text-lg font-normal font-['Baloo Da 2'] leading-7">
-            <a href="#home">Home</a>
+      <div className="w-screen hidden h-[49px] md:block relative right-[50vh] top-4">
+        <div className="h-[29px] left-0 top-[15px]">
+          <div className="left-0 top-0 absolute hover:text-red cursor-pointer text-lg font-normal font-Baloo leading-7">
+            <a href="/">Home</a>
           </div>
-          <div className="left-[87px] top-0 absolute text-black hover:text-red cursor-pointer text-lg font-normal font-['Baloo Da 2'] leading-7">
+          <div className="left-[87px] top-0 absolute text-black hover:text-red cursor-pointer text-lg font-normal font-Baloo leading-7">
             <a href="#services">Services</a>
           </div>
-          <div className="left-[199px] top-0 absolute text-black hover:text-red cursor-pointer text-lg font-normal font-['Baloo Da 2'] leading-7">
+          <div className="left-[199px] top-0 absolute text-black hover:text-red cursor-pointer text-lg font-normal font-Baloo leading-7">
             <a href="#about">About Us</a>
           </div>
-          <div className="left-[307px] top-0 absolute text-black hover:text-red cursor-pointer text-lg font-normal font-['Baloo Da 2'] leading-7">
+          <div className="left-[307px] top-0 absolute text-black hover:text-red cursor-pointer text-lg font-normal font-Baloo leading-7">
             <a href="#contacts">Contacts</a>
           </div>
         </div>
