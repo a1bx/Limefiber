3c0874a196d1f86e0e3083ee5bf075e9bfe17b22
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-03-26 12:10:01 +0300

    blog update

--- src/components/blog-selected.jsx
+++ src/components/blog-selected.jsx
@@ -4,6 +4,8 @@ import Footer from './footer';
 import Banner3 from '../assets/banner3.svg';
 import ProfilePic1 from '../assets/user1.svg';
 import ProfilePic2 from '../assets/user4.svg';
+import { BsFillReplyFill } from "react-icons/bs";
+
 
 const Blog = () => {
   const [formData, setFormData] = useState({
@@ -38,37 +40,38 @@ const Blog = () => {
     <>
       <Navbar />
       <div className="bg-white p-4">
-        <h1 className="md:text-3xl text-xl font-bold text-center mb-6">Trendy Topics on Home Fiber Technology</h1>
+        <h1 className="md:text-3xl text-xl font-bold text-center mb-6 font-Baloo">Trendy Topics on Home Fiber Technology</h1>
         <img src={Banner3} alt="Blog" className="mb-4 w-full object-cover rounded-lg md:w-screen md:h-[69vh] lg:px-20" />
         <div className="max-w-4xl mx-auto">
           <div className="mb-8">
-            <div className="text-lg font-semibold mb-2">Title of the Blog</div>
-            <div className="text-gray-600 mb-4">Author Name</div>
-            <p className="text-gray-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit arcu quis nisi finibus, vel sollicitudin nisi tincidunt. Integer vestibulum interdum ligula, nec varius felis interdum eu. Sed sollicitudin, ligula eu convallis consequat, arcu elit vestibulum mi, a commodo nulla tellus vel purus. Donec sollicitudin bibendum ex, ac tincidunt orci fermentum sit amet. Nulla facilisi. Nullam non felis dui.</p>
+            <div className="text-lg font-semibold mb-2 font-Baloo">Title of the Blog</div>
+            <div className="text-gray-600 mb-4 font-Baloo">Author Name</div>
+            <p className="text-gray-800 font-Baloo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit arcu quis nisi finibus, vel sollicitudin nisi tincidunt. Integer vestibulum interdum ligula, nec varius felis interdum eu. Sed sollicitudin, ligula eu convallis consequat, arcu elit vestibulum mi, a commodo nulla tellus vel purus. Donec sollicitudin bibendum ex, ac tincidunt orci fermentum sit amet. Nulla facilisi. Nullam non felis dui.</p>
           </div>
 
           <div className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">Comments</h2>
+            <h2 className="text-2xl font-bold mb-4 font-Baloo">Comments</h2>
             <div className="space-y-4">
               <div className="flex space-x-4">
                 <img src={ProfilePic1} alt="Profile 1" className="w-10 h-10 rounded-full" />
                 <div>
-                  <div className="font-semibold">John Doe</div>
-                  <p className="text-gray-800">Great blog post!</p>
+                  <div className="font-semibold font-Baloo">John Doe</div>
+                  <div className='justify-item-end ml-20 mb-10' ><BsFillReplyFill /></div>
+                  <p className="text-gray-800 font-Baloo">Great blog post!</p>
                 </div>
               </div>
               <div className="ml-12 flex space-x-4">
                 <img src={ProfilePic2} alt="Profile 2" className="w-10 h-10 rounded-full" />
                 <div>
-                  <div className="font-semibold">Jane Smith</div>
-                  <p className="text-gray-800">Thanks, John! I agree.</p>
+                  <div className="font-semibold font-Baloo">Jane Smith</div>
+                  <p className="text-gray-800 font-Baloo">Thanks, John! I agree.</p>
                 </div>
               </div>
             </div>
           </div>
 
           <div>
-            <h2 className="text-2xl font-bold mb-4">Leave a Comment</h2>
+            <h2 className="text-2xl font-semibold font-Baloo mb-4">Leave a Comment</h2>
             <form className="space-y-4" onSubmit={handleSubmit}>
               <input
                 type="text"
@@ -96,19 +99,19 @@ const Blog = () => {
                 className="w-full px-4 py-2 font-Baloo rounded-lg focus:outline-none focus:border-blue-500"
                 placeholder="Your Phone"
               />
-              <label className="flex items-center">
+              <label className="ml-4 flex items-center">
                 <input
                   type="checkbox"
                   name="rememberEmail"
                   checked={formData.rememberEmail}
                   onChange={handleChange}
-                  className="form-checkbox h-5 w-5 text-blue-600"
+                  className="form-checkbox h-3 w-3 text-green"
                 />
-                <span className="ml-2 text-gray font-Baloo">Remember Email</span>
+                <span className="ml-2 font-Baloo text-xs">Remember Email</span>
               </label>
               <button
                 type="submit"
-                className="bg-green text-white font-semibold py-2 px-4 rounded hover:bg-lightgreen focus:outline-none focus:bg-green"
+                className="bg-green text-white font-semibold py-2 px-4 rounded hover:bg-lightgreen font-Baloo focus:outline-none focus:bg-green"
               >
                 Post Comment
               </button>
