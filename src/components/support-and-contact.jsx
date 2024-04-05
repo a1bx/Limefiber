ab78bb7b41649f592a8dab76e7b04c526b3adf8b
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-04-05 23:55:01 +0300

    update

--- src/components/support-and-contact.jsx
+++ src/components/support-and-contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import { FaMapMarkerAlt, FaPhone, FaEnvelope } from 'react-icons/fa';
 import Navbar from './navbar';
 import Search from '../assets/search.svg';
@@ -7,10 +7,26 @@ import { AiOutlineRight,AiOutlineDown } from 'react-icons/ai';
 import Livechat from '../assets/livechat.svg';
 
 const ContactPage = () => {
+  const [email, setEmail] = useState('');
+  // const [rememberEmail, setRememberEmail] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  // const handleRememberEmailChange = () => {
+  //   setRememberEmail(!rememberEmail);
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Implement your submission logic here
+  //   console.log("Submitted!");
+  // };
   return (
     <div className="container mx-auto">
       <Navbar />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 md:p-10 gap-8">
         <div className="w-72 h-8 left-[16px] relative md:hidden">
           <input
             className="left-[30px] w-72 h-8 top-[6px] absolute text-sm font-normal font-Baloo leading-tight border-none bg-gray rounded"
@@ -23,30 +39,51 @@ const ContactPage = () => {
             alt=""
           />
         </div>
-        <div className="bg-white rounded-lg shadow-md p-6">
+        <div className="bg-white rounded-lg p-6">
           <p className='font-Baloo font-bold text-2xl'>Support & Contact</p>
           <hr className="my-3 w-1/3 border border-gray" />
           <h2 className="text-xl mb-8 font-Baloo">Contact Information</h2>
-          <div className=" mb-4 ">
-            <FaMapMarkerAlt className="text-gray-600 mr-2" style={{ fontSize: "1.8em"}}/>
+          <div className=" mb-4 grid md:grid-cols-2">
+            <FaMapMarkerAlt className="text-gray mr-2" style={{ fontSize: "1.8em"}}/>
             <p className="font-black mb-4 font-Baloo">Address</p>
             <p className='text-start mt-4'>0000 Road,  Nairobi 00100, Kenya</p>
+            <input
+            type="email"
+            placeholder="Enter your name"
+            value={email}
+            onChange={handleEmailChange}
+            className="border-0 border-b-2 px-3 py-2 mb-2 w-full"
+          />
             {/* <input></input> */}
           </div>
-          <div className="mb-4">
-            <FaPhone className="text-gray-600 mr-2" style={{ fontSize: "1.8em"}}/>
+          <div className="mb-4 grid md:grid-cols-2">
+            <FaPhone className="text-gray mr-2" style={{ fontSize: "1.8em"}}/>
             <p className="font-black mb-4 font-Baloo">Phone</p>
             <p>Mobile: 0000 1111 2222</p>
             <p>Hotline: 0000 3333 4444</p>
+            <input
+            type="email"
+            placeholder="Enter your message"
+            value={email}
+            onChange={handleEmailChange}
+            className="border-0 border-b-2 px-3 py-2 mb-2"
+          />
           </div>
           <div className="mt-4">
-            <FaEnvelope className="text-gray-600 mr-2" style={{ fontSize: "1.8em"}} />
+            <FaEnvelope className="text-gray mr-2" style={{ fontSize: "1.8em"}} />
             <p className="font-black mb-4 font-Baloo">Email Address</p>
             <p>abc@gmail.com</p>
             <p>giftly_xyz@gmail.com</p>
+            <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleEmailChange}
+            className="border-0 border-b-2 px-3 py-2 mb-2 basis-1/2"
+          />
           </div>
         </div>
-        <div className="bg-white rounded-lg shadow-md p-6">
+        <div className="bg-white rounded-lg p-6">
           <h2 className="text-2xl font-bold mb-4 font-Baloo">FAQs</h2>
           <div className="mb-4 mt-10 shadow-lg rounded-md p-4">
             <h3 className="text-lg  mb-2 font-semibold font-Baloo">What is Limefiber?
